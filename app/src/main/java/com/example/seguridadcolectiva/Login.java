9cca2ee9a8ce65f43df9aa03e628a769993aaa81
Jose A. Huertas <josehuertaspty@gmail.com>
2023-07-19 07:45:29 -0500

    Agregando logica para la clase login y modificando un id en el activity_login

--- app/src/main/java/com/example/seguridadcolectiva/Login.java
+++ app/src/main/java/com/example/seguridadcolectiva/Login.java
@@ -1,37 +1,70 @@
 package com.example.seguridadcolectiva;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    private EditText etPassword, etUsermail;
+    private Button btnLogin;
+    private DatabaseHelper databaseHelper;
+
+    private int loginAttempts = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    public void Login(View view){
-        Intent i = new Intent(this,MainMain.class);
-        startActivity(i);
-    }
+        etUsermail = findViewById(R.id.etlUsermail);
+        etPassword = findViewById(R.id.etlPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        TextView textLink = findViewById(R.id.text_link_registro);
 
-    public void back(View view){
-        Intent i = new Intent(this,MainActivity.class);
-        startActivity(i);
+        databaseHelper = new DatabaseHelper(this);
 
-    }
-    public void Sign(View view){
-        Intent i = new Intent(this,Sign_up.class);
-        startActivity(i);
-    }
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etUsermail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                boolean success = databaseHelper.loginUser(email, password);
+                if (success) {
+                    // Inicio de sesión exitoso, realizar la acción deseada
+                    Toast.makeText(Login.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                    String[] data = databaseHelper.getNameAndLastNameAndFields(email);
 
-    public void resetpass(View view){
-        Intent i = new Intent(this,Reset_Pass.class);
-        startActivity(i);
+                    Intent intent = new Intent(Login.this, MainMain.class);
+                    intent.putExtra("email", email);
+                    startActivity(intent);
+
+                } else {
+                    // Inicio de sesión fallido, incrementar el contador de intentos
+                    loginAttempts++;
+                    if (loginAttempts >= 3) {
+                        Toast.makeText(Login.this, "Por favor, regístrate para continuar", Toast.LENGTH_LONG).show();
+                    } else {
+                        // Inicio de sesión fallido, mostrar mensaje de error
+                        Toast.makeText(Login.this, "Credenciales inválidas, intenta nuevamente", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        textLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Acción a realizar cuando se hace clic en el texto
+                Intent intent = new Intent(Login.this, Sign_up.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
