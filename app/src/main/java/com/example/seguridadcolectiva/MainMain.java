dfedbc0f72c24a45be29326bd364303387a75f7a
Alexis Aparicio <alexiiz360@gmail.com>
2023-07-19 15:21:37 -0500

    add form and main

--- app/src/main/java/com/example/seguridadcolectiva/MainMain.java
+++ app/src/main/java/com/example/seguridadcolectiva/MainMain.java
@@ -6,10 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,72 +20,24 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
 public class MainMain extends AppCompatActivity {
-    Location mLastLocation;
-    TextView mLatitude;
-    TextView mLongitude;
-    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_main);
-        mLatitude=(TextView)findViewById(R.id.ubicacion);
-        //mLongitude=(TextView)findViewById(R.id.mLongitude);
-
-        // Establecer punto de entrada para la API de ubicación
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
-                .addOnConnectionFailedListener((GoogleApiClient.OnConnectionFailedListener) this)
-                .addApi(LocationServices.API)
-                .build();
-
-
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
+    public void Formulario(View view){
+        Intent i = new Intent(this,Formulario.class);
+        startActivity(i);
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mGoogleApiClient.disconnect();
+    public void Sign(View view){
+        Intent i = new Intent(this,Sign_up.class);
+        startActivity(i);
     }
 
-    public class LocationActivity extends AppCompatActivity implements
-            GoogleApiClient.ConnectionCallbacks,
-            GoogleApiClient.OnConnectionFailedListener {
-        @Override
-        public void onConnected(@Nullable Bundle bundle) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if (mLastLocation != null) {
-                mLatitude.setText(String.valueOf(mLastLocation.getLatitude()));
-                mLongitude.setText(String.valueOf(mLastLocation.getLongitude()));
-            } else {
-                Toast.makeText(this, "Ubicación no encontrada", Toast.LENGTH_LONG).show();
-            }
-        }
-
-        @Override
-        public void onConnectionSuspended(int i) {
-
-        }
-
-        @Override
-        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-        }
+    public void End(View view){
+        finish();
     }
 }
